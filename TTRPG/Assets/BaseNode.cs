52b4a8417dd29f25c4240f27161798b521635b8f
walts-14 <walterjohne14@gmail.com>
2025-05-25 23:20:52 +0800

    Reapply "1"
    This reverts commit 00d1197c993ce4bf6494931e1dce2c8f6e70043c.

---
+++ TTRPG/Assets/BaseNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class BaseNode : Node {
+
+	public virtual string getDialogText()
+	{
+		return "";
+	}
+
+	public virtual Sprite getSprite()
+	{
+		return null;
+	}
+	public virtual ABILITY getAbility()
+	{
+		return ABILITY.AWARENESS;
+	}
+	public virtual float getDifficultyCheck()
+	{
+		return 10.0f;
+	}
+}
---
+++ TTRPG/Assets/SimpleDialog.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public enum MusicChoice {
+    adventure,
+    happy,
+    suspense,
+    drama
+}
+
+public class SimpleDialog : BaseNode {
+
+    [Input] public string entry;
+    [Output] public string exit;
+
+    [TextArea(7, 20)]
+    public string DialogText;
+    public Sprite BackgroundImage;
+    
+    // 🎭 Actor Image Placeholder
+    public Sprite ActorImage;
+    
+    // 🎬 Animate Actor Image (Checkbox)
+    public bool animateActor;
+
+    // 🎵 Dropdown to select background music
+    public MusicChoice backgroundMusicChoice;
+
+    // ✅ Define a default background to prevent null issues
+    public static Sprite defaultBackground;
+
+    public override string getDialogText() {
+        return DialogText;
+    }
+
+    public override Sprite getSprite() {
+        return BackgroundImage != null ? BackgroundImage : defaultBackground;
+    }
+
+    public AudioClip getBackgroundMusic() {
+        return MusicManager.Instance.GetMusic(backgroundMusicChoice);
+    }
+
+    // 🎵 Play selected background music
+    public void PlaySelectedMusic() {
+        MusicManager.Instance.PlayMusic(backgroundMusicChoice);
+    }
+}
